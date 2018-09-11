9e4e9f2fbb1d8cf4a70cf4626d642aea1f89e162
Abishak Kodi <abishak.kodi@gmail.com>
2018-09-10 20:01:21 -0700

    Added more templates Fake data for future DB

---
+++ data.py
@@ -0,0 +1,24 @@
+def Articles():
+  articles =[
+  {
+  'id':1,
+  'title': 'article1',
+  'body': 'LOREM IPSO UPSO FUPSO FACTOOO'
+
+  },
+  {
+  'id':2,
+  'title': 'article2',
+  'body': 'LOREM IPSO UPSO FUPSO FACTOOO'
+
+  }
+  ,
+  {
+  'id':3,
+  'title': 'article3',
+  'body': 'LOREM IPSO UPSO FUPSO FACTOOO'
+
+  }
+  ]
+  return articles
+
